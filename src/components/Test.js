afc5f6bb7134b9064a79293a25ae01839eab40d7
sleny <xkqortjddl@naver.com>
2020-09-08 20:47:56 +0900

    dataShow1

--- src/components/Test.js
+++ src/components/Test.js
@@ -1,25 +1,86 @@
 import React from "react";
 import axios from "axios";
+import "../Css/compnentesCss/Test.css";
 class Test extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       data: "",
+      search: "",
+      number: "",
+      backUrl: "",
+      name: "",
+      detailon: false,
     };
   }
   componentDidMount() {
     // this.getData();
   }
-
-  getData = async () => {
-    const data = await axios.get("http://18.221.28.100:4000/users");
-    this.setState(data);
-    console.log(data);
+  handleDetail = (number, backUrl, name) => {
+    this.setState({ number, backUrl, name });
+    this.setState({ detailon: true });
   };
-
+  handleChange = (e) => {
+    this.setState({
+      search: e.target.value,
+    });
+  };
   render() {
+    const searchData = this.props.data ? (
+      this.props.data.data
+        .filter((info) => {
+          if (this.state.search == null) return this.props.data;
+          else if (
+            info.cntntsSj
+              .toLowerCase()
+              .includes(this.state.search.toLowerCase())
+          ) {
+            return this.props.data;
+          }
+        })
+        .map((list) => {
+          const cut = list.rtnStreFileNm.split("|");
+          const imgUrl = `http://www.nongsaro.go.kr/cms_contents/301/${cut[0]}`;
+          const backUrl = `http://www.nongsaro.go.kr/cms_contents/301/${cut[1]}`;
+          return (
+            <div className="ShowCss" key={list.cntntsNo}>
+              <li
+                className="DataLi"
+                key={list.cntntsNo}
+                onClick={() =>
+                  this.handleDetail(list.cntntsNo, backUrl, list.cntntsSj)
+                }
+              >
+                <img
+                  src={imgUrl}
+                  width="230"
+                  height="230"
+                  alt={list.cntntsSj}
+                />
+                <div className="DataTitle" key={list.cntntsNo}>
+                  {list.cntntsSj}
+                </div>
+              </li>
+            </div>
+          );
+        })
+    ) : (
+      <div>....Loading</div>
+    );
     return (
-      <div>{this.props.data ? this.props.data.data[0].cntntsNo : "false"}</div>
+      <div>
+        <input
+          className="InputType"
+          value={this.state.search}
+          name="search"
+          onChange={this.handleChange}
+          placeholder="🔍Search ..."
+        ></input>
+
+        <div className="PhotoBox">
+          <ul className="DataUl">{searchData}</ul>
+        </div>
+      </div>
     );
   }
 }
